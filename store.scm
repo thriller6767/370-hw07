44a4c40e0c53d35e950a247584a8e54d9fc33fad
Ruili Tang <Ruili Tang>
2017-03-05 22:34:35 -0500

    store list -> vector. Passed simple test of new-ref and grow size

--- store.scm
+++ store.scm
@@ -4,25 +4,53 @@
 
 ;; the-store! is the store!
 (define the-store! 'uninitialized)
+(define current-p 0)
 
 ;; (empty-store) return an empty Scheme list representing the empty
 ;; store.
 (define empty-store
 	(lambda ()
-		'())
+		(make-vector 20)))
 
 ;; (initialize-store!) it initializes the-store! to (empty-store)
 (define initialize-store!
 	(lambda ()
 		(set! the-store! (empty-store))))
 
-;; (newref! val) takes a value val adds to the the-store!, and returns
-;; a ref-val to the added value val.
+;; (newref! val) 
+;; if current pointer is smaller to size(there is space to add a new ref), 
+;; takes a value val adds to the the-store!, and returns a ref-val to the added value val.
+;; else, grow the size of store, add the val and return ref-val
 (define newref!
 	(lambda (val)
-		(set! the-store! (append the-store! (list val)))
-		(ref-val (- (length the-store!) 1))
-))
+		(cond
+			[ (< current-p (vector-length the-store!))
+				(vector-set! the-store! current-p val)
+				(set! current-p (+ current-p 1))
+				(ref-val (- current-p 1))]
+			[else
+				(set! the-store! (vector-grow the-store! 
+											 (make-vector (+ current-p 20))
+											  0))
+				(vector-set! the-store! current-p val)
+				(set! current-p (+ current-p 1))
+				(ref-val (- current-p 1))
+		]
+	)))
+
+;;copy every val of vec to new-vec, return new-vec
+;;note: the length of new-vec must >= length of vec
+(define vector-grow
+	(lambda (vec new-vec curr)
+		(cond
+			[(< curr (vector-length vec))
+				(vector-set! new-vec curr (vector-ref vec curr))  ;copy the val at index curr
+				(vector-grow vec new-vec (+ curr 1))]  ;recur on the next index
+			[else
+					new-vec
+			 ]
+		)))
+	
 
 ;; (deref ev) expects that ev is a reference (ref-val ref), and
 ;; returns the value of associated with ref in the store.
@@ -30,7 +58,7 @@
 	(lambda (ev)
 		(let 
 			([ref (expval->ref ev)])
-			(list-ref the-store! ref))))
+			(vector-ref the-store! ref))))
 
 ;; (setref! ev val) expects that ev is a reference (ref-val ref), and
 ;; it sets the reference ref to val in the the-store!
@@ -38,13 +66,4 @@
 	(lambda (ev val)
 		(let
 			([ref (expval->ref ev)])
-			(set! the-store! (setref!* the-store! ref val)))))
-
-;; (setref!* store ref val) should return the store where var has been set to val.
-;; ref is an integer index into the store.  0 is the first element of the store and
-;; (- (length store) 1) is the index of the last element of the store.
-(define setref!*
-	(lambda (store ref val)
-		(cond
-			[(= 0 ref) (cons val (cdr store))]
-			[else (cons (car store) (setref!* (cdr store) (- ref 1) val))])))
+			(vector-set! the-store! ref val))))
